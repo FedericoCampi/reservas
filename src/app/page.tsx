effc2a24b7f77339cf7cf5ad8fc46171b9e9c015
FedericoCampi <federicocampi95@gmail.com>
2024-04-13 20:40:37 +0100

    ending

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,33 @@ import Categories from "@/components/customComponents.tsx/categories";
 import SelectTime from "@/components/customComponents.tsx/selectTime";
 import Confirm from "@/components/customComponents.tsx/confirm";
 import ShiftConfirmed from "@/components/customComponents.tsx/ShiftConfirmed";
+import servicesData from '@/mocks/services.json';
+import slots from '@/mocks/slots.json';
+import { format } from 'date-fns';
 
 export default function Home() {
 
   const [seleccionado, setSeleccionado] = useState(false);
   const [showInterfaces, setShowInterfaces] = useState(0);
   const [selectTime, setSelectTime] = useState(false);
-  const titles = ['Select service', 'Select schedule', 'Confirm shift'];
+  const titles = ['Select service', 'Select schedule', 'Confirm appointment'];
   const [selectedCategoryId, setSelectedCategoryId] = useState(0);
   const [selectedSchedule, setSelectedSchedule] = useState('');
   const progress = [25, 50, 75, 100];
+  const [section, setSection] = useState(0);
+  const [appointmentConfirmed, setAppointmentConfirmed] = useState(0);
 
-  const handleSelectChange = (categoryId:number) => {
-    if(categoryId == 0){
+  const handleSelectChange = (categoryId: number) => {
+    if (categoryId == 0) {
       setSeleccionado(false);
-    }else{
+    } else {
       setSeleccionado(true);
     }
     setSelectedCategoryId(categoryId);
   };
 
-  const handleSelectSchedule= (schedule:string) => {
-    if(schedule !== ''){
+  const handleSelectSchedule = (schedule: string) => {
+    if (schedule !== '') {
       setSelectTime(true)
     }
   };
@@ -43,109 +48,152 @@ export default function Home() {
     setShowInterfaces(showInterfaces + 1);
   };
 
-  useEffect(() => {
+  const goToAppointments = () => {
+    setSection(1);
+  };
 
-    // if(showInterfaces == 0 && seleccionado == false){
-    //   setSeleccionado(false);
-    // }
+  const service = servicesData.services.find(service=> service.id === selectedCategoryId);
+  const fechaOriginal = slots.date;
+  const fecha = new Date(fechaOriginal);
+  const fechaFormateada = format(fecha, 'dd/MM/yyyy');
+
+  const turnoConfirmado = () => {
+    setAppointmentConfirmed(1);
+  }
+
+  const anotherAppointment = () => {
+    window.history.replaceState({}, '', '/');
+    window.location.reload();
+  }
+
+  useEffect(() => {
 
     if (typeof window !== 'undefined') {
 
-      // const queryParams = new URLSearchParams(router.asPath.split('?')[1]); // Utilizamos router.asPath en lugar de window.location.search
-      //   const selectedCategory = Number(queryParams.get('category'));
       const queryParams = new URLSearchParams(window.location.search);
       const selectedCategory = Number(queryParams.get('category'));
 
       const selectedSchedule = queryParams.get('slot');
 
       if (selectedCategory > 0) {
-          setSeleccionado(true);
-          setSelectedCategoryId(selectedCategory);
+        setSeleccionado(true);
+        setSelectedCategoryId(selectedCategory);
 
-          if(selectedSchedule){
-            setSelectedSchedule(selectedSchedule);
-          }
-      }else{
+        if (selectedSchedule) {
+          setSelectedSchedule(selectedSchedule);
+        }
+      } else {
         if (showInterfaces === 0 && !seleccionado) {
           setSeleccionado(false);
         }
       }
-      
     }
 
-  },[showInterfaces, seleccionado]);
+  }, [showInterfaces, seleccionado]);
 
   return (
     <main className={`bg-neutral-200 min-h-screen w-full pb-20 ${seleccionado && ' pb-40 '}`}>
-      
-        <div className="h-[15%] w-full">
-            <div className="p-6">
-              <p>{titles[showInterfaces]}</p>
-              <Progress value={progress[showInterfaces]} />
-          </div>
-        </div>
-        <div className="h-[65%] w-full px-8">
-
-          {showInterfaces === 0 ? (
-              <Categories onSelectChange={handleSelectChange}/>
-            ) : showInterfaces === 1 ? (
-              <SelectTime onSelectSchedule={handleSelectSchedule}/>
-            ) : showInterfaces === 2 ? (
-              <Confirm category={selectedCategoryId} schedule={selectedSchedule}/>
-            ) : (
-              <ShiftConfirmed/>
-          )}
-        </div>
-        <div className="w-full fixed bottom-0 bg-neutral-200">
-          {
-            seleccionado && showInterfaces !== 3 ? (
-              <div className="w-full flex justify-between px-8 py-2 border-t border-b border-black">
-                {
-                  showInterfaces == 0 ? (
-                    <div></div>
-                  ) : (
-                    <button onClick={goBack}
-                      className="bg-neutral-800 text-white p-2 hover:bg-neutral-500 hover:text-black">
-                      Back
-                    </button>
-                  )
-                }
-                {
-                  showInterfaces == 2 ? (
-                    <button onClick={goNext}
-                      className="bg-neutral-800 text-white p-2 hover:bg-neutral-500 hover:text-black"
-                    >
-                        Confirm
-                    </button>
-                  ) : (
-                    <button onClick={goNext}
-                      disabled={selectTime == false && showInterfaces !== 0}
-                      className={`${selectTime == false && showInterfaces !== 0 ? 'bg-neutral-400 text-black p-2' : 'bg-neutral-800 text-white p-2 hover:bg-neutral-500 hover:text-black'} '`}>
-                        Next
-                    </button>
-                  )
-                }
+      {
+        section == 0 ? (
+          <div>
+            <div className="h-[15%] w-full">
+              <div className="p-6">
+                <p>{titles[showInterfaces]}</p>
+                <Progress value={progress[showInterfaces]} />
               </div>
-
-            ) : (
-              <div>
-
-              </div>
-            )
-          }
-          <div className="w-full flex justify-center space-x-10 py-1">
-            <div className="flex flex-col items-center border-b-2 border-[#1934B6]">
-              <TableCellsIcon width={30} color={'#1934B6'}/>
-              <p className="text-blue-800">Reserve</p>
             </div>
-            <div className="flex flex-col items-center">
-              <TableCellsIcon width={30}/>
-              <p>My shifts</p>
+            <div className="h-[65%] w-full px-8">
+
+              {showInterfaces === 0 ? (
+                <Categories onSelectChange={handleSelectChange} />
+              ) : showInterfaces === 1 ? (
+                <SelectTime onSelectSchedule={handleSelectSchedule} />
+              ) : showInterfaces === 2 ? (
+                <Confirm category={selectedCategoryId} schedule={selectedSchedule} />
+              ) : (
+                <ShiftConfirmed goToAppointments={goToAppointments}/>
+              )}
             </div>
+            
           </div>
-        </div>
+        ) : (
+          appointmentConfirmed == 1 ? (
+            <div className="h-[500px] w-full flex flex-col justify-between">
+              <div className="border border-1 border-black h-auto w-full px-4 py-5">
+                <p>Service: {service?.name}</p>
+                <p>Date: {fechaFormateada} {selectedSchedule}</p>
+              </div>
+              <div className="w-auto text-center">
+                <button onClick={anotherAppointment}
+                      className='mt-6 p-2 bg-green-700 text-black hover:bg-green-900 hover:text-white'>
+                      Make another <br /> appointments
+                </button>
+              </div>
+            </div>
+            
+          ) : (
+            <div className="border border-1 border-black h-auto w-full px-4 py-5">
+              No appointments reserved
+            </div>
+          )
+          
+        )
+      }
+          <div className="w-full fixed bottom-0 bg-neutral-200">
+              {
+                seleccionado && section == 0 && showInterfaces !== 3 ? (
+                  <div className="w-full flex justify-between px-8 py-2 border-t border-black">
+                    {
+                      showInterfaces == 0 ? (
+                        <div></div>
+                      ) : (
+                        <button onClick={goBack}
+                          className="bg-neutral-800 text-white p-2 hover:bg-neutral-500 hover:text-black">
+                          Back
+                        </button>
+                      )
+                    }
+                    {
+                      showInterfaces == 2 ? (
+                        <button onClick={() => {
+                              goNext();
+                              turnoConfirmado();
+                          }}
+                          className="bg-neutral-800 text-white p-2 hover:bg-neutral-500 hover:text-black"
+                        >
+                          Confirm
+                        </button>
+                      ) : (
+                        <button onClick={goNext}
+                          disabled={selectTime == false && showInterfaces !== 0}
+                          className={`${selectTime == false && showInterfaces !== 0 ? 'bg-neutral-400 text-black p-2' : 'bg-neutral-800 text-white p-2 hover:bg-neutral-500 hover:text-black'} '`}>
+                          Next
+                        </button>
+                      )
+                    }
+                  </div>
 
-      
+                ) : (
+                  <div>
+
+                  </div>
+                )
+              }
+              <div className={`w-full flex justify-center space-x-10 py-1 border-t border-1 border-black`}>
+                <div onClick={() => { setSection(0) }}
+                  className={`flex flex-col items-center ${section == 0 ? 'border-b-2 border-[#1934B6]' : ''}`}
+                >
+                  <TableCellsIcon width={30} color={section == 0 ? '#1934B6' : ''} />
+                  <p className={section == 0 ? 'text-blue-800' : ''}>Reserve</p>
+                </div>
+                <div onClick={() => { setSection(1) }}
+                  className={`flex flex-col items-center ${section == 0 ? '' : 'border-b-2 border-[#1934B6]'}`}
+                >
+                  <TableCellsIcon width={30} color={section == 0 ? '' : '#1934B6'} />
+                  <p className={section == 0 ? '' : 'text-blue-800'}>Appointments</p>
+                </div>
+              </div>
+            </div>
     </main>
   );
 }
--- src/components/customComponents.tsx/ShiftConfirmed.tsx
+++ src/components/customComponents.tsx/ShiftConfirmed.tsx
@@ -1,10 +1,28 @@
 import Image from 'next/image';
 import successGif from '../../../public/successGIF.gif';
 
-const ShiftConfirmed = () => {
+const ShiftConfirmed = ({ goToAppointments }: { goToAppointments: () => void }) => {
+
+    const anotherAppointment = () => {
+        window.history.replaceState({}, '', '/');
+        window.location.reload();
+    }
+
     return(
-        <div>
-            <Image src={successGif} alt="successGif" />
+        <div className='min-h-screen w-full'>
+            <div className='min-h-screen w-full flex justify-center items-center flex-col pb-[250px]'>
+                <Image src={successGif} alt="successGif" />
+                <p className='pb-16'>Appointment confirmed</p>
+                <button onClick={goToAppointments}
+                    className='p-2 bg-neutral-800 text-white hover:bg-neutral-600 hover:text-black'>
+                    My appointments
+                </button>
+                <button onClick={anotherAppointment}
+                    className='mt-6 p-2 bg-green-700 text-black hover:bg-green-900 hover:text-white'>
+                    Make another <br /> appointments
+                </button>
+            </div>
+            
         </div>
     )
 }
